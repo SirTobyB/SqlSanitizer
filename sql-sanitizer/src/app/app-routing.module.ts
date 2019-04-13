3ce156836b1ff11b7fa4991e8fa54662ca8e0368
Julius Hell <jhell2306@googlemail.com>
2019-04-13 11:55:24 +0200

    updated routes

--- sql-sanitizer/src/app/app-routing.module.ts
+++ sql-sanitizer/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
