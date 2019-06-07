90289cc3e77e8a5453ee2dc590ae0e0c7ac64448
Julius Hell <jhell2306@googlemail.com>
2019-06-07 18:47:06 +0200

    improved ui

--- sql-sanitizer/src/app/app.module.ts
+++ sql-sanitizer/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SqlParameterComponent } from './sql-parameter/sql-parameter.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SqlParameterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
